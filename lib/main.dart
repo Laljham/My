3b2288cb4ebe2c63d33be507f07c754775dde9b1
Laljham <divyajyoti51072@gmail.com>
2025-11-28 20:37:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,79 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-const apiKey = 'YOUR_API_KEY';
-const playlistId = 'YOUR_PLAYLIST_ID';
+void main() {
+  runApp(const TodoApp());
+}
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp();
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PlaylistHome(),
       debugShowCheckedModeBanner: false,
+      home: TodoHome(),
     );
   }
 }
 
-class PlaylistHome extends StatefulWidget {
-  const PlaylistHome({Key? key}) : super(key: key);
-
+class TodoHome extends StatefulWidget {
   @override
-  State<PlaylistHome> createState() => _PlaylistHomeState();
+  State<TodoHome> createState() => _TodoHomeState();
 }
 
-class _PlaylistHomeState extends State<PlaylistHome> {
-  List videos = [];
+class _TodoHomeState extends State<TodoHome> {
+  List<Map<String, dynamic>> todos = [];
+  final TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    fetchPlaylist();
+    loadTodos();
   }
 
-  Future<void> fetchPlaylist() async {
-    final url =
-        'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=60&playlistId=$playlistId&key=$apiKey';
-    final res = await http.get(Uri.parse(url));
-    final data = json.decode(res.body);
+  /// Load Todos from SharedPreferences
+  Future<void> loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? data = prefs.getString("todos");
+    if (data != null) {
+      setState(() {
+        todos = List<Map<String, dynamic>>.from(json.decode(data));
+      });
+    }
+  }
+
+  /// Save Todos
+  Future<void> saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("todos", json.encode(todos));
+  }
+
+  /// Add Task
+  void addTodo() {
+    if (controller.text.trim().isEmpty) return;
     setState(() {
-      videos = data['items'];
+      todos.add({"task": controller.text.trim(), "done": false});
     });
+    controller.clear();
+    saveTodos();
+  }
+
+  /// Toggle Complete
+  void toggleDone(int index) {
+    setState(() {
+      todos[index]["done"] = !todos[index]["done"];
+    });
+    saveTodos();
+  }
+
+  /// Delete Task
+  void deleteTodo(int index) {
+    setState(() {
+      todos.removeAt(index);
+    });
+    saveTodos();
   }
 
   @override
@@ -51,65 +81,77 @@ class _PlaylistHomeState extends State<PlaylistHome> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('My Playlist'),
-        backgroundColor: Colors.redAccent,
+        title: const Text("Todo App"),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: videos.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: videos.length,
-              itemBuilder: (context, i) {
-                final snippet = videos[i]['snippet'];
-                final videoId = snippet['resourceId']['videoId'];
-                return Card(
-                  color: Colors.grey[900],
-                  child: ListTile(
-                    leading: Image.network(snippet['thumbnails']['default']['url']),
-                    title: Text(snippet['title'],
-                        style: const TextStyle(color: Colors.white)),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => VideoPlayerScreen(videoId: videoId),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(Icons.add),
+        onPressed: addTodo,
+      ),
+
+      body: Column(
+        children: [
+          // Text Input
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: controller,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: "Add a task...",
+                hintStyle: const TextStyle(color: Colors.white54),
+                filled: true,
+                fillColor: Colors.grey[900],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+          ),
+
+          // List of Tasks
+          Expanded(
+            child: todos.isEmpty
+                ? const Center(
+                    child: Text(
+                      "No tasks yet!",
+                      style: TextStyle(color: Colors.white70),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: todos.length,
+                    itemBuilder: (context, index) {
+                      final task = todos[index];
+
+                      return Card(
+                        color: Colors.grey[900],
+                        child: ListTile(
+                          leading: Checkbox(
+                            value: task["done"],
+                            onChanged: (_) => toggleDone(index),
+                            activeColor: Colors.deepPurple,
+                          ),
+                          title: Text(
+                            task["task"],
+                            style: TextStyle(
+                              color: Colors.white,
+                              decoration: task["done"]
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.redAccent),
+                            onPressed: () => deleteTodo(index),
+                          ),
                         ),
                       );
                     },
                   ),
-                );
-              },
-            ),
-    );
-  }
-}
-
-class VideoPlayerScreen extends StatefulWidget {
-  final String videoId;
-  const VideoPlayerScreen({required this.videoId});
-
-  @override
-  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
-}
-
-class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late YoutubePlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = YoutubePlayerController(
-      initialVideoId: widget.videoId,
-      flags: const YoutubePlayerFlags(autoPlay: true),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: YoutubePlayer(
-        controller: _controller,
-        showVideoProgressIndicator: true,
+          ),
+        ],
       ),
     );
   }
